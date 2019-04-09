12d2e6827f69e651f3afc181ee993e9eddaf9bd9
Olly Britton <ollybritton@gmail.com>
2019-04-09 14:36:12 +0100

    Add func command for manual entry

--- jump-config/cmd/func.go
+++ jump-config/cmd/func.go
@@ -23,28 +23,39 @@ import (
 // funcCmd represents the func command
 var funcCmd = &cobra.Command{
 	Use:   "func",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Output the function for the given options",
+	Long:  `Print out the function associated with the given options, so you can add the function to your config file manually.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("func called")
+		language, err := cmd.Flags().GetString("language")
+		if err != nil {
+			fmt.Println("Cannot find specified language. Use the --language flag to specify either bash or zsh.")
+			return
+		}
+
+		if language != "bash" && language != "zsh" {
+			fmt.Println("That is not a supported language. Try either 'zsh' or 'bash'.")
+			return
+		}
+
+		color, err := cmd.Flags().GetBool("color")
+		if err != nil {
+			fmt.Println("There was a problem getting the color flag.")
+			return
+		}
+
+		function, err := GetShellFunction(language, color)
+		if err != nil {
+			fmt.Printf("Error getting shell function: %v\n", err)
+		}
+
+		fmt.Println(function)
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(funcCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// funcCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// funcCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	funcCmd.Flags().StringP("language", "l", "zsh", "What language to use for the shell function.")
+	funcCmd.Flags().BoolP("color", "c", false, "Use color for the command")
 }
